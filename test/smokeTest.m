6c188db301d34bb14630bbcd18d6c6ca973868fa
gulley <gulley@mathworks.com>
2023-02-16 16:17:31 -0500

    Add asserts to smokeTest.m

--- test/smokeTest.m
+++ test/smokeTest.m
@@ -1,21 +1,33 @@
 %% Smoke Test
 
 h = animator;
+assert(isequal(h.Animator.Name,"MATLAB Animator"))
 
 %%
 
-% FAILING
-[var,val,rng] = splitCode("x = 12;  %  1..20", 17);
+[sweepVar, startVal, sweepRange] = splitCode("x = 12;  %  1..20");
+assert(isequal(sweepVar,"x"))
+assert(isequal(startVal,12))
+assert(isequal(sweepRange,[1 20]))
 
 %%
 
-[var,val,rng] = splitCode("result = 17;");
+[sweepVar, startVal, sweepRange] = splitCode("result = 17;");
+assert(isequal(sweepVar,"result"))
+assert(isequal(startVal,17))
+assert(isequal(sweepRange,[]))
 
 %%
 
-[var,val,rng] = splitCode("result = -20.17;");
+[sweepVar, startVal, sweepRange] = splitCode("result = -20.17;");
+assert(isequal(sweepVar,"result"))
+assert(isequal(startVal,-20.17))
+assert(isequal(sweepRange,[]))
 
 %%
 
-[var,val,rng] = splitCode("plot(1:10)");
+[sweepVar, startVal, sweepRange] = splitCode("plot(1:10)");
+assert(isequal(sweepVar,""))
+assert(isequal(startVal,[]))
+assert(isequal(sweepRange,[]))
 
